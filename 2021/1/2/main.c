35d0a8a991d1833c7ba123f270379b8d7aa9de78
OnikenX <OnikenX@pm.me>
2021-12-01 18:05:27 +0000

    added an debug mode to the make file

--- 2021/1/2/main.c
+++ 2021/1/2/main.c
@@ -3,59 +3,70 @@
 
 #define INPUT_FILE_NAME "input.txt"
 
-void addIntToArray(int** arrayPointer, int* size, const int newNumber) {
-    int* tempArray = realloc(*arrayPointer, (*size + 1) * sizeof(int));
+void addIntToArray(int **arrayPointer, int *size, const int newNumber) {
+  int *tempArray = realloc(*arrayPointer, (*size + 1) * sizeof(int));
 
-    if (tempArray == NULL) {
-        fprintf(stderr, "ERROR: unable to allocate memory for array of ints.");
-        return;
-    }
+  if (tempArray == NULL) {
+    fprintf(stderr, "ERROR: unable to allocate memory for array of ints.");
+    return;
+  }
 
-    *arrayPointer = tempArray;
-    (*arrayPointer)[*size] = newNumber;
-    (*size)++;
+  *arrayPointer = tempArray;
+  (*arrayPointer)[*size] = newNumber;
+  (*size)++;
 }
 
-int readInputIntFromTxt(int** arrayPointer, char* fileName) {
-    int size = 0;
-    FILE *inputFile = NULL;
+int readInputIntFromTxt(int **arrayPointer, char *fileName) {
+  int size = 0;
+  FILE *inputFile = NULL;
 
-    inputFile = fopen(fileName, "rt");
+  inputFile = fopen(fileName, "rt");
 
-    if (inputFile == NULL) {
-        fprintf(stderr, "ERROR: unable to read file %s.", fileName);
-        return -1;
-    }
-    
-    int temp;
+  if (inputFile == NULL) {
+    fprintf(stderr, "ERROR: unable to read file %s.", fileName);
+    return -1;
+  }
 
-    while (fscanf(inputFile, "%i", &temp) == 1) {
-        addIntToArray(arrayPointer, &size, temp);
-    }
+  int temp;
 
-    fclose(inputFile);
-    return size;
+  while (fscanf(inputFile, "%i", &temp) == 1) {
+    addIntToArray(arrayPointer, &size, temp);
+  }
+
+  fclose(inputFile);
+  return size;
 }
 
-int nrTimeDepthIncreases(const int* array, const int size, const int window) {
-    int number = 0;
+int nrTimeDepthIncreases(const int *array, const int size, const int window) {
+  int number = 0;
 
-    for (int i = 0; i < size - window; i++) {
-        if (array[i] < array[i + window]) number++;
-    }
+  for (int i = 0; i < size - window; i++) {
+    if (array[i] < array[i + window])
+      number++;
+  }
 
-    return number;
+  return number;
 }
 
+#include <sys/time.h>
 int main() {
-    int* array = NULL;
-    int arraySize;
 
-    arraySize = readInputIntFromTxt(&array, INPUT_FILE_NAME);
+  struct timeval timer[2];
+  gettimeofday(&timer[0], NULL);
 
-    int nrDepthIncreases = nrTimeDepthIncreases(array, arraySize, 3);
-    printf("Number of times a depth measurement increases is %i.\n", nrDepthIncreases);
+  int *array = NULL;
+  int arraySize;
 
-    free(array);
-    return 0;
+  arraySize = readInputIntFromTxt(&array, INPUT_FILE_NAME);
+
+  int nrDepthIncreases = nrTimeDepthIncreases(array, arraySize, 3);
+  printf("Number of times a depth measurement increases is %i.\n",
+         nrDepthIncreases);
+
+  free(array);
+
+  gettimeofday(&timer[1], NULL);
+
+  printf("time taken: %ld μs\n", timer[1].tv_usec - timer[0].tv_usec);
+  return 0;
 }
